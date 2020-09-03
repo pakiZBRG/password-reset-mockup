86efec03ff6be5c38dd4428b4f9f0f3dcea6831e
pakiZBRG <pavlovicnikola511@gmail.com>
2020-09-03 16:53:13 +0200

    mysql

--- database.php
+++ database.php
@@ -1,5 +1,9 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "forgotten_pass");
+<?php
+    //Localhost 
+    // $conn = mysqli_connect("localhost", "root", "", "forgotten_pass");
+
+    //Live
+    $conn = mysqli_connect("sql7.freemysqlhosting.net", "sql7363547", "gQzHsTPck7", "sql7363547");
 
     if(mysqli_connect_errno()){
         echo "Failed to connect: ".mysqli_connect_errno();
